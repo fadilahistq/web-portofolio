d6a4d33a6020a9d4cf7a54bb5cfda3526d354c5f
Fadilah Istiqamah <fadilahistiqomah0206@gmail.com>
2024-05-05 23:10:03 +0800

    task web portofolio

---
+++ main.js
@@ -0,0 +1,49 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+	menuIcon.classList.toggle("fa-xmark");
+	navbar.classList.toggle("active");
+	if (navbar.classList.contains("active")) {
+		navbar.style.display = "block";
+	} else {
+		navbar.style.display = "none"; 
+	}
+
+};
+
+// Scroll section active link
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+	sections.forEach(sec => {
+		let top = window.scrollY;
+		let offset = sec.offsetTop - 150;
+		let height = sec.offsetHeight;
+		let id = sec.getAttribute("id");
+
+		if (top >= offset && top < offset + height) {
+			navLinks.forEach.apply(links => {
+				links.classList.remove("active");
+				document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+			});
+		};
+	});
+
+	// sticky navbar
+	let header = document.querySelector("header");
+	header.classList.toggle("sticky", window.scrollY > 100);
+	// remove toggle icon and navbar
+	menuIcon.classList.remove("fa-xmark");
+	navbar.classList.remove("active");
+};
+
+// // Typed js
+const typed = new Typed('.multiple-text', {
+	strings: ['UI/UX Designer', 'Web Designer', 'Frontend Developer'],
+	typeSpeed: 70,
+	backSpeed: 70,
+	backDelay: 1000,
+	loop: true,
+});
